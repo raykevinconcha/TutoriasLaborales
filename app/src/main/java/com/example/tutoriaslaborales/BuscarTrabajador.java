b79a969facba43481ab6a086cd580eccf6ffa5cb
raykevinconcha <a20186137@pucp.edu.pe>
2023-10-23 14:48:04 -0500

    Buscar

---
+++ app/src/main/java/com/example/tutoriaslaborales/BuscarTrabajador.java
@@ -0,0 +1,14 @@
+package com.example.tutoriaslaborales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuscarTrabajador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar_trabajador);
+    }
+}
