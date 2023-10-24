35e8f9d81fac300bd3d744784337ada780927d50
weekings <a20190212@pucp.edu.pe>
2023-10-23 21:43:19 -0500

    descagr en tutor

--- app/src/main/java/com/example/tutoriaslaborales/Codigo_tutor.java
+++ app/src/main/java/com/example/tutoriaslaborales/Codigo_tutor.java
@@ -1,10 +1,19 @@
 package com.example.tutoriaslaborales;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.EntityIterator;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,7 +26,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Codigo_tutor extends AppCompatActivity {
-    private static final int REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,54 +33,37 @@ public class Codigo_tutor extends AppCompatActivity {
 
         Button descarga = findViewById(R.id.descarga);
         descarga.setOnClickListener(view -> {
-            if (ContextCompat.checkSelfPermission(Codigo_tutor.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                // Si no lo tienes, solicita el permiso
-                ActivityCompat.requestPermissions(Codigo_tutor.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
-            } else {
-                // Si ya tienes el permiso, puedes crear y escribir en el archivo
-                crearYEsribirArchivo();
-            }
+            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TITLE, "lista.txt");
+            activityForResultLauncher.launch(intent);
+
         });
     }
 
-    private void crearYEsribirArchivo() {
-        // Define el nombre del archivo y la ruta
-        String fileName = "mi_archivo.txt";
-        String filePath = Environment.getExternalStorageDirectory().getPath() + File.separator + fileName;
+    ActivityResultLauncher<Intent> activityForResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
 
-        // Crea un objeto File
-        File file = new File(filePath);
+                    if (data != null) {
+                        try (ParcelFileDescriptor pfd = getContentResolver().openFileDescriptor(data.getData(), "w");
+                             FileWriter fileWriter = new FileWriter(pfd.getFileDescriptor())) {
+                            EditText texto = findViewById(R.id.codigo);
+                            String textoGuardar = texto.getText().toString();
+                            fileWriter.write("ID del tutor"+textoGuardar+
+                                    "\n Empleados que son manager:"+
+                                    "\n Jorge Benavides" +
+                                    "\n Joaquin Carhuaz"+
+                                    "\n Alvaro Calle");
+                            Toast.makeText(this, "Archivo guardado", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
 
-        try {
-            // Crea el archivo si no existe
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            // Abre un FileWriter para escribir en el archivo
-            FileWriter writer = new FileWriter(file);
-            EditText editText = findViewById(R.id.codigo); // Obtén una referencia al EditText
-
-            String valorIngresado = editText.getText().toString();
-            // Escribe el contenido en el archivo
-            writer.write(valorIngresado);
-
-            // Cierra el FileWriter
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permiso concedido, crea y escribe en el archivo
-                crearYEsribirArchivo();
-            } else {
-                Toast.makeText(Codigo_tutor.this, "no tienes permiso de almanacenamiento", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 }
